b89a22e97c3ac4eeccf3d7baeecd8985405a7b1f
gouxuenan <33390002+gouxuenan@users.noreply.github.com>
2021-07-20 09:57:37 +0800

    Add files via upload

---
+++ bsf.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(corrplot)
+library(psych)
+
+setwd("")
+
+df <- read_tsv("")
+
+A <- filter(df, layer == "A")
+
+layerA <- filter(df, layer == "A") %>%
+  select(!layer & !ID & !type & !group) %>%
+  corr.test(use = "complete", method = "pearson", adjust = "none")
+
+corrplot(layerA$r, p.mat = layerA$p, insig ="label_sig",sig.level = c(0.001, 0.01, 0.05), pch.cex = 1.5, pch.col = "white", type = "upper", tl.pos = "d", tl.col = "black")
+
+layerB <- filter(df, layer == "B") %>%
+  select(!layer & !ID & !type & !group) %>%
+  corr.test(use = "complete", method = "pearson", adjust = "none")
+
+corrplot(layerB$r, p.mat = layerB$p, insig ="label_sig",sig.level = c(0.001, 0.01, 0.05), pch.cex = 1.5, pch.col = "white", type = "upper", tl.pos = "d", tl.col = "black")
+
+
+typeYS <- filter(df, type == "YS") %>%
+  select(!layer & !ID & !type & !group) %>%
+  corr.test(use = "complete", method = "pearson", adjust = "none")
+corrplot(typeYS$r, p.mat = typeYS$p, insig ="label_sig",sig.level = c(0.001, 0.01, 0.05), pch.cex = 1.5, pch.col = "white", type = "upper", tl.pos = "d", tl.col = "black")
+
+typeLYS <- filter(df, type == "LYS") %>%
+  select(!layer & !ID & !type & !group) %>%
+  corr.test(use = "complete", method = "pearson", adjust = "none")
+corrplot(typeLYS$r, p.mat = typeLYS$p, insig ="label_sig",sig.level = c(0.001, 0.01, 0.05), pch.cex = 1.5, pch.col = "white", type = "upper", tl.pos = "d", tl.col = "black")
